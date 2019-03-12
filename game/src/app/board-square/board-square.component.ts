f0e3f1b53ae30fd34de434cf596935c5e11d9a1d
Erin Denae <erindenae@erins-mbp.lan>
2019-03-12 16:17:07 -0500

    update gameboard

---
+++ game/src/app/board-square/board-square.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-square',
+  templateUrl: './board-square.component.html',
+  styleUrls: ['./board-square.component.scss']
+})
+export class BoardSquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
